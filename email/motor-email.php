9051d96f722a212ad13aadb1261b0f30e03de14d
ableSebik <40537291+ableSebik@users.noreply.github.com>
2023-01-08 23:27:07 +0000

    Update motor-email.php

--- email/motor-email.php
+++ email/motor-email.php
@@ -22,7 +22,7 @@ try {
     $mail->Host       = 'mail.tunodes.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'sebikabel@tunodes.com';                     //SMTP username
-    $mail->Password   = 'yinmisomma1';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
