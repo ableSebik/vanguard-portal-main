14ec9d1cbe737f1eaf77b3bc940ae269c01d0000
ableSebik <40537291+ableSebik@users.noreply.github.com>
2023-01-27 16:48:00 +0000

    new

--- controller/process-motor-claim.php
+++ controller/process-motor-claim.php
@@ -60,5 +60,56 @@ foreach($medicalReports['name'] as $key=>$val){
 
 //////////////////////////////////////////////////
 //send mail if there's no error
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+// require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'mail.tunodes.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'sebikabel@tunodes.com';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('sebikabel@tunodes.com');
+    $mail->addAddress('fakulti47@gmail.com');     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('sebikabel@gmail.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('../uploads/attach1.jpg');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Claim for Motor Insurance';
+    $mail->Body    = '<h2>Test message from phpmailer <br/>it worked</h2>';
+    $mail->AltBody = 'PHPMailer test';
+
+    $mail->send();
+    // echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+//using a test email test@tunodes.com password = _CUFiT+0D$ZB
+//mail.tunodes.com  IMAP Port: 993 POP3 Port: 995
 ?>
