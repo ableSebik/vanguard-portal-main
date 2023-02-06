701e8bbfff156b75a5eb2056fd1cc8f623b87f6c
ableSebik <fakulti47@gmail.com>
2023-02-06 18:41:47 +0000

    smtp cred removed

--- controller/process-motor-claim.php
+++ controller/process-motor-claim.php
@@ -473,17 +473,17 @@ try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = "mail.tunodes.com";                     //Set the SMTP server to send through
+    $mail->Host       = "";                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'test@tunodes.com';                     //SMTP username
+    $mail->Username   = '';                     //SMTP username
     $mail->Password   = "AVEPAr8wm*cPypg";                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
     //Recipients
-    $mail->setFrom("test@tunodes.com");
+    $mail->setFrom("");
     // echo "this is the email add ".getenv("mailHost");
-    $mail->addAddress("test@tunodes.com");     //Add a recipient
+    $mail->addAddress("");     //Add a recipient
     // $mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('');
     // $mail->addBCC('bcc@example.com');
